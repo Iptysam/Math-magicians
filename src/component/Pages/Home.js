7f570473908e52e498158824574c9bfc1468e0a1
Iptysam <mukadamiptisam@gmail.com>
2022-11-02 17:28:41 +0300

    Create pages for the website

---
+++ src/component/Pages/Home.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './Pages.css';
+
+const Home = () => (
+  <div>
+    <h1 className="Home_title">
+      Math Magicians
+    </h1>
+    <h2 className="Home_info">
+      Welcome to Our Page
+    </h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam amet
+      consequuntur possimus, nulla id praesentium porro voluptatum
+      laboriosam rerum ullam ipsa. Iusto alias quas, distinctio iste
+      voluptates assumenda exercitationem obcaecati.Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Aliquam amet
+      consequuntur possimus, nulla id praesentium porro voluptatum
+      laboriosam rerum ullam ipsa. Iusto alias quas, distinctio iste
+      voluptates assumenda exercitationem obcaecati.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam amet
+      consequuntur possimus, nulla id praesentium porro voluptatum
+      laboriosam rerum ullam ipsa. Iusto alias quas, distinctio iste voluptates
+      assumenda exercitationem obcaecati
+      .Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam amet
+      consequuntur possimus, nulla id praesentium porro voluptatum
+      laboriosam rerum ullam ipsa. Iusto alias quas, distinctio iste
+      voluptates assumenda exercitationem obcaecati.
+    </p>
+  </div>
+);
+
+export default Home;
