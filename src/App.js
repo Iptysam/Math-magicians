02d37656808af3d92f8270dcde80951d3eaa72a5
Iptysam <mukadamiptisam@gmail.com>
2022-11-02 17:29:35 +0300

    Update the app file with the pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './index.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Calculator from './component/Calculator';
+import Home from './component/Pages/Home';
+import Navbar from './component/Pages/navbar';
+import Qoute from './component/Pages/qoute';
 
 class App extends React.PureComponent {
   constructor(props) {
@@ -10,10 +14,14 @@ class App extends React.PureComponent {
 
   render() {
     return (
-      <div>
-        <h1>Do your Calculations</h1>
-        <Calculator />
-      </div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="src/component/Pages/Home.js" element={<Home />} />
+          <Route path="/component/Calculator" element={<Calculator />} />
+          <Route path="src/component/Pages/qoute.js" element={<Qoute />} />
+        </Routes>
+      </Router>
     );
   }
 }
