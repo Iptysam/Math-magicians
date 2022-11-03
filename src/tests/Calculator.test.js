b4e5e4dde72e561226a99d96599b229775163388
Iptysam <mukadamiptisam@gmail.com>
2022-11-03 16:29:43 +0300

    Create a test for calculator to display value

---
+++ src/tests/Calculator.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Calculator from '../component/Calculator';
+
+it('It should render the display of button in the screen', () => {
+  const screen = renderer
+    .create(< Calculator />)
+    .toJSON();
+  expect(screen).toMatchSnapshot();
+});
